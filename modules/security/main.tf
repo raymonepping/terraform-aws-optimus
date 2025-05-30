7910f5e48c30bca579c1e8f569c4375f744564f7
Raymon Epping <your@email.com>
2025-05-30 11:05:49 +0200

    Additional settings for security

--- modules/security/main.tf
+++ modules/security/main.tf
@@ -1,13 +1,35 @@
+variable "ingress_rules" {
+  type = list(object({
+    from_port   = number
+    to_port     = number
+    protocol    = string
+    cidr_blocks = list(string)
+    description = optional(string)
+  }))
+  default = [
+    {
+      from_port   = 22
+      to_port     = 22
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  ]
+}
+
 resource "aws_security_group" "ssh" {
   name        = "allow-ssh"
-  description = "Allow SSH inbound"
+  description = "Allow defined ingress rules"
   vpc_id      = var.vpc_id
 
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = var.allowed_ssh_cidrs
+  dynamic "ingress" {
+    for_each = var.ingress_rules
+    content {
+      from_port   = ingress.value.from_port
+      to_port     = ingress.value.to_port
+      protocol    = ingress.value.protocol
+      cidr_blocks = ingress.value.cidr_blocks
+      description = lookup(ingress.value, "description", null)
+    }
   }
 
   egress {
--- modules/security/outputs.tf
+++ modules/security/outputs.tf
@@ -1,3 +1,4 @@
 output "ssh_security_group_id" {
-  value = aws_security_group.ssh.id
+  description = "The ID of the SSH security group"
+  value       = aws_security_group.ssh.id
 }
